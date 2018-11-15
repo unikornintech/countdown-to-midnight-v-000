0164ab6a0e6a531cf94a29b52ec893fb0f2d32eb
Lauren Days <lmchambers0420@gmail.com>
2018-11-15 02:29:42 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -3,7 +3,7 @@ countdown = 10
 
 def countdown(seconds)
   while seconds > 0 
-    puts " #{seconds} SECOND(S)!" 
+    puts " #{numbers} SECOND(S)!" 
     seconds -= 1
   end
     return "HAPPY NEW YEAR!"
