97e946b624bb5a81b8be4af578b9256a0d0a956f
Lauren Days <lmchambers0420@gmail.com>
2018-11-15 02:12:47 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown(seconds)
+  while seconds > 0 
+    puts " #{seconds} SECOND(S)!" 
+    seconds -= 1
+  end
+    return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(seconds)
+  while seconds > 0 
+    puts "#{seconds} SECOND(S)"
+    sleep 1 
+    seconds -= 1 
+  end
+    return "HAPPY NEW YEAR!"
 end
